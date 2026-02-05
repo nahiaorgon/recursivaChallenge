765910e8d4c50131575371929a417dd53f8aa112
Nahiara Orgoñ <norgon@desarrollosnea.com>
2026-02-05 02:26:10 -0300

    [Client] Visualizacion de la respuesta proveniente de la api.

--- Horoscopo.Api/Horoscopo.Client/Program.cs
+++ Horoscopo.Api/Horoscopo.Client/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<DatoRegistro>();
+builder.Services.AddScoped<Registro>();
 
 builder.Services.AddScoped(sp => new HttpClient
 {
