b8bad1311a20b4a68c41abcf46bae65b6d141aa1
Nahiara Orgoñ <norgon@desarrollosnea.com>
2026-02-05 23:10:51 -0300

    alcance general del appsetting para la url de la api

--- Horoscopo.Api/Horoscopo.Client/Program.cs
+++ Horoscopo.Api/Horoscopo.Client/Program.cs
@@ -4,13 +4,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<Registro>();
 
+var apiBaseUrl = builder.Configuration["ApiUrl"];
+
+if (string.IsNullOrEmpty(apiBaseUrl))
+{
+    apiBaseUrl = builder.HostEnvironment.BaseAddress;
+}
+
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7124/")
+    BaseAddress = new Uri(apiBaseUrl)
 });
 
 await builder.Build().RunAsync();
